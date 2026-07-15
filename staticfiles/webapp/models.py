e73d2b5da82c9110f4ee30792e29878e937f6faa
Allanods <kabiraallan79@gmail.com>
2026-07-15 11:59:31 +0300

    Initial commit

---
+++ staticfiles/webapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    message = models.TextField()
+    category = models.CharField(max_length=50, default='General')
+
+    def __str__(self):
+         return self.name
